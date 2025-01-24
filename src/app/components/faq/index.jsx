8162ddb0e1e0b4889cc164cdf4ff0433dc1e9227
Shiva pandey <shivapandey9898@gmail.com>
2025-01-24 18:51:39 +0530

    Update index.jsx

--- src/app/components/faq/index.jsx
+++ src/app/components/faq/index.jsx
@@ -42,7 +42,7 @@ export default function FAQ(){
 
     return (
 
-        <section className="py-14 lg:py-20">
+        <section className="py-14 lg:py-20" id="faq">
             <Container>
                 <div className="grid grid-cols-1 lg:grid-cols-2 gap-6 md:gap-16">
                     <div className="w-full h-full ">
